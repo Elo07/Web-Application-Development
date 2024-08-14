0ea1bc5699983bcf7f35746abf7e09474bbf35a0
Larry Okuonghae <elokuonghae@myseneca.ca>
2024-08-14 02:22:06 -0400

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
- *  WEB322 – Assignment 05
+ *  WEB322 – Assignment 06
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part
  *  of this assignment has been copied manually or electronically from any other source
  *  (including 3rd party web sites) or distributed to other students.
  *
- *  Name: Larry Okuonghae Student ID: 145203238 Date: 07/31/2024
+ *  Name: Larry Okuonghae Student ID: 145203238 Date: 08/14/2024
  *
  *  Vercel Web App URL: https://web322-app-six-umber.vercel.app/
  *
